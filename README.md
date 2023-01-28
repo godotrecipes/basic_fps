e9c00459a9540113d47d49a214f41d3151dedb1f
Chris Bradfield <cb@scribe.net>
2023-01-28 13:00:42 -0800

    initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Basic FPS Character
+
+![alt](http://kidscancode.org/godot_recipes/4.x/img/fps_01.png)
+
+Tutorial:
+http://kidscancode.org/godot_recipes/4.x/3d/basic_fps/
---
+++ fps_player.gd
@@ -0,0 +1,28 @@
+extends CharacterBody3D
+
+var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
+var speed = 5
+var jump_speed = 5
+var mouse_sensitivity = 0.002
+
+
+func _input(event):
+	if event is InputEventMouseMotion and Input.mouse_mode == Input.MOUSE_MODE_CAPTURED:
+		rotate_y(-event.relative.x * mouse_sensitivity)
+		$Camera3D.rotate_x(-event.relative.y * mouse_sensitivity)
+		$Camera3D.rotation.x = clampf($Camera3D.rotation.x, -deg_to_rad(70), deg_to_rad(70))
+		
+func _physics_process(delta):
+	velocity.y += -gravity * delta
+	var input = Input.get_vector("left", "right", "forward", "back")
+	var movement_dir = transform.basis * Vector3(input.x, 0, input.y)
+	if movement_dir.length() != 0:
+		$AnimationPlayer.play("walk")
+	else:
+		$AnimationPlayer.play("idle")
+	velocity.x = movement_dir.x * speed
+	velocity.z = movement_dir.z * speed
+	
+	move_and_slide()
+	if is_on_floor() and Input.is_action_just_pressed("jump"):
+		velocity.y = jump_speed
